3e7267d8d598b2418ed77b499c0eb86f7e158d30
zake7749 <zake7749@gmail.com>
2015-05-18 18:37:13 +0800

    add project file of Eclipse and Chess.java

---
+++ src/Chess.java
@@ -0,0 +1,29 @@
+
+public abstract class Chess {
+	
+	public Chess(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	
+	
+	
+	private int x,y;
+}
