151747e928aab225d3e439f5a9525a2a2114351e
Zake <zake7749@gmail.com>
2015-05-21 21:37:01 +0800

    enhance determineGrid in Game.java

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -15,6 +16,7 @@ public class Game extends JFrame implements MouseListener{
 	private JPanel contentPane;
 	private JLabel DebugText;
 	private String debugMessage;
+	private Chess[][] chessBoard;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -32,7 +34,7 @@ public class Game extends JFrame implements MouseListener{
 
 	public Game() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 600);
+		setBounds(150, 10, 620, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -43,12 +45,16 @@ public class Game extends JFrame implements MouseListener{
 		DebugText.setVerticalAlignment(SwingConstants.BOTTOM);
 		contentPane.add(DebugText, BorderLayout.SOUTH);
 		
+		JLabel chessBoardpic = new JLabel("");
+		chessBoardpic.setIcon(new ImageIcon("ChessBoard.png"));
+		contentPane.add(chessBoardpic, BorderLayout.CENTER);
+		
 	}
 	
 	private Point determineGrid(int x,int y){
 		
-		int chessBoardX=30,chessBoardY=50;
-		int gridWidth=40,gridHeight=40;
+		int chessBoardX=24,chessBoardY=41;
+		int gridWidth=70,gridHeight=70;
 		
 		int gx = (x - chessBoardX)/gridWidth;
 		int gy = (y - chessBoardY)/gridHeight;
@@ -59,9 +65,9 @@ public class Game extends JFrame implements MouseListener{
 
 	public void mouseClicked(MouseEvent e) {
 		
-		determineGrid(e.getX(),e.getY());
+		Point p = determineGrid(e.getX(),e.getY());
 		//DEBUG
-		debugMessage = new String("Clicked. X is: " + e.getX() + " Y is: " + e.getY());
+		debugMessage = new String("Clicked. X is: " + e.getX() + " Y is: " + e.getY() + ". Grid is :[" + p.y + "," + p.x +"]");
 		DebugText.setText(debugMessage);
 		//DEBUG
 	}
