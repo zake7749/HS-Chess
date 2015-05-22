fa27448f356012fd1859e88ae933a81e29400f11
Zake <zake7749@gmail.com>
2015-05-22 22:32:26 +0800

    add foundationa lstrcut of AI

---
+++ src/AI.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+
+
+public class AI {
+	
+	private boolean strikeFever;
+	private int comp;
+	private Stack bestChoices;
+	
+	public AI(int comp){
+		this.comp = comp;
+	}
+	
+	public int miniMax(Chess[][] drivedChessBoard,int depth){
+		int score = 0;
+		
+		return score;
+	}
+	
+	public void changeImage(JLabel lbl){
+		
+	}
+	
+	public void say(JTextPane txt){
+		
+	}
+	
+
+}
