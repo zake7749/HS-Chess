425f0ff37c36dd1dda231846e740115a673e1750
Justin Yang <zake7749@gmail.com>
2015-06-06 15:57:29 +0800

    add Bishop isReachable

--- src/Bishop.java
+++ src/Bishop.java
@@ -30,29 +30,87 @@ public class Bishop extends Chess {
 		
 		for(i=0; i<8; i++)
 		{
-			if(x-i >= 0 && y-i >= 0 && x+i < 8 && y+i < 8)
+			if(x-i >= 0 && y-i>=0 && chessboard[x-i][y-i] == null)
 			{
-				if(chessboard[x-i][y-i] != null)
-					reachable[x-i][y-i] = true;
-				
-				if(chessboard[x+i][y-i] != null)
-					reachable[x+i][y-i] = true;
-				
-				if(chessboard[x-i][y+i] != null)
-					reachable[x-i][y+i] = true;
-				
-				if(chessboard[x+i][y+i] != null)
-					reachable[x+i][y+i] = true;
+				reachable[x-i][y-i] = true;
 			}
+			else if (chessboard[x-i][y-i].camp != camp)
+			{
+				reachable[x-i][y-i] = true;
+				break;
+			}
+			
+			else break;
+		}
+		
+		for(i=0; i<8; i++)
+		{
+			if(x-i >= 0 && y+i < 8 && chessboard[x-i][y+i] == null)
+			{
+				reachable[x-i][y+i] = true;
+			}
+			else if (chessboard[x-i][y+i].camp != camp)
+			{
+				reachable[x-i][y+i] = true;
+				break;
+			}
+			
+			else break;
+		}
+		
+		for(i=0; i<8; i++)
+		{
+			if(x+i < 8 && y-i>=0 && chessboard[x+i][y-i] == null)
+			{
+				reachable[x+i][y-i] = true;
+			}
+			else if (chessboard[x+i][y-i].camp != camp)
+			{
+				reachable[x+i][y-i] = true;
+				break;
+			}
+			
+			else break;
+		}
+		
+		for(i=0; i<8; i++)
+		{
+			if(x+i < 8 && y+i < 8 && chessboard[x+i][y+i] == null)
+			{
+				reachable[x+i][y+i] = true;
+			}
+			else if (chessboard[x+i][y+i].camp != camp)
+			{
+				reachable[x+i][y+i] = true;
+				break;
+			}
+			
+			else break;
 		}
 		
 		
+		reachable[x][y] = false;
 		
 		return reachable;
 	}
-	public boolean isReachable(Chess[][]chessboard,int x,int y)
+	public boolean isReachable(Chess[][]chessboard,int Ix,int Iy)
 	{
+		boolean reach = false;
+		boolean[][] reachable;
+		int i, j;
 		
+		reachable = getReachableGrid(chessboard);
+		
+		for(i=0; i<8; i++)
+		{
+			for(j=0; j<8; j++)
+			{
+				if (reachable[i][j] == true && Ix == i && Iy == j)
+					reach = true;
+			}
+		}
+		
+		return reach;
 	}
  
 	public JLabel icon;
